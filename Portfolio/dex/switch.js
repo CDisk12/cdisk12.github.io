21b36a9a7780201b3cacae555202fb48396f0327
Sypier <113011224+CDisk12@users.noreply.github.com>
2023-03-01 21:07:34 +0800

    Add files via upload

---
+++ Portfolio/dex/switch.js
@@ -0,0 +1,16 @@
+function $bgcolor() {
+	let checkbox = document.getElementById('box');
+	let $0 = document.getElementById('$0');
+	let $1 = document.getElementById('$1');
+	if (checkbox.checked == true) {
+		document.body.style.backgroundColor = '#141414';
+		document.body.style.transition = "all 1s";
+		$0.style.display = "none";
+		$1.style.display = "block";
+	} else {
+		document.body.style.backgroundColor = "#ffffff";
+		document.body.style.transition = "all 1s";
+		$0.style.display = "block";
+		$1.style.display = "none";
+	}
+}
